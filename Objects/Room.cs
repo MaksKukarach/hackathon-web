59dda2706f652d0c012710bc4b030bb229e1dc6f
DiscoMike <samoulov.max.18@gmail.com>
2023-02-18 02:48:03 +0300

    room class

---
+++ Objects/Room.cs
@@ -0,0 +1,24 @@
+﻿namespace hackathon.Objects
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public double Area { get; set; }
+        public double MonthlyRent { get; set; }
+        public bool HasExpiredPayments { get; set; }
+        public string Type { get; set; } = "";
+        public string Tenant { get; set; } = "";
+        public List<string>? Equipment { get; set; }
+        public double MonthlyIncome { get; set; }
+        public double TotalIncome { get; set; }
+        public DateTime LatestInspection { get; set; }
+        public DateTime StartContract { get; set; }
+        public DateTime EndContract { get; set; }
+        public string? ImgSrc { get; set; }
+
+        public Room()
+        {
+
+        }
+    }
+}
