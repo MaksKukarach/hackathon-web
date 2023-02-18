3d052f877c9dca1803e3b23d9f118c670f796462
DiscoMike <samoulov.max.18@gmail.com>
2023-02-18 15:27:12 +0300

    rooms catalog

--- Objects/Room.cs
+++ Objects/Room.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public double Area { get; set; }
-        public double MonthlyRent { get; set; }
+        public double MonthlyRent { get; set; } = 0;
         public bool HasExpiredPayments { get; set; }
         public string Type { get; set; } = "";
         public string Tenant { get; set; } = "";
@@ -14,7 +14,8 @@
         public DateTime LatestInspection { get; set; }
         public DateTime StartContract { get; set; }
         public DateTime EndContract { get; set; }
-        public string? ImgSrc { get; set; }
+        public string ImgSrc { get; set; } = "";
+        public string Link { get; set; } = "";
 
         public Room()
         {
