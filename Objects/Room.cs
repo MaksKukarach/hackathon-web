872e6d4d7f16b3b20f5390f96dff66da47c05114
DiscoMike <samoulov.max.18@gmail.com>
2023-02-18 18:18:43 +0300

    total objects + statistics

--- Objects/Room.cs
+++ Objects/Room.cs
@@ -7,7 +7,7 @@
         public double MonthlyRent { get; set; } = 0;
         public bool HasExpiredPayments { get; set; }
         public string Type { get; set; } = "";
-        public string Tenant { get; set; } = "";
+        public string? Tenant { get; set; }
         public List<string>? Equipment { get; set; }
         public double MonthlyIncome { get; set; }
         public double TotalIncome { get; set; }
