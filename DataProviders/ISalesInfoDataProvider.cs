9932d277678019b57b768b362477778a0d9f6ce4
DiscoMike <samoulov.max.18@gmail.com>
2023-02-19 05:07:19 +0300

    cards

---
+++ DataProviders/ISalesInfoDataProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using hackathon.Data;
+
+namespace hackathon.DataProviders
+{
+    public interface ISalesInfoDataProvider
+    {
+        Task<IEnumerable<SaleInfo>> GetSalesAsync(CancellationToken ct = default);
+        Task<IEnumerable<SaleInfo>> GetReducedSalesAsync(CancellationToken ct = default);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using hackathon.DataProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+//builder.Services.AddSingleton<ISalesInfoDataProvider>();
 
 var app = builder.Build();
 
